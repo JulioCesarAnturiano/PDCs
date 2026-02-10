960f5ad175b0f70e9513430692a7a437f09d0a54
JulioCesarAnturiano <juliocesaranturianoeulate@gmail.com>
2026-02-10 01:20:39 -0400

    modelos de todas las tablas

---
+++ app/Models/VotoGeografico.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class VotoGeografico extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'voto_geografico';
+    protected $primaryKey = 'id_geografico';
+
+    protected $fillable = [
+        'nombre',
+        'codigo',
+        'ubicacion',
+        'tipo',
+        'fk_id_geografico',
+    ];
+
+    const TIPO_PAIS = 'PAIS';
+    const TIPO_CIUDAD = 'CIUDAD';
+    const TIPO_MUNICIPIO = 'MUNICIPIO';
+    const TIPO_LOCALIDAD = 'LOCALIDAD';
+    const TIPO_RECINTO = 'RECINTO';
+
+    public function padre()
+    {
+        return $this->belongsTo(VotoGeografico::class, 'fk_id_geografico', 'id_geografico');
+    }
+
+    public function hijos()
+    {
+        return $this->hasMany(VotoGeografico::class, 'fk_id_geografico', 'id_geografico');
+    }
+
+    public function mesas()
+    {
+        return $this->hasMany(VotoMesa::class, 'id_recinto', 'id_geografico');
+    }
+}
---
+++ app/Models/VotoUsuario.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class VotoUsuario extends Authenticatable
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'voto_usuario';
+    protected $primaryKey = 'id_usuario';
+    protected $guard_name = 'sanctum';
+
+    protected $fillable = [
+        'nombre_usuario',
+        'contrasena',
+        'fecha_fin',
+        'token',
+    ];
+
+    protected $hidden = [
+        'contrasena',
+        'token',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'fecha_fin' => 'date',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'id_usuario';
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->contrasena;
+    }
+
+    public function setContrasenaAttribute($value)
+    {
+        $this->attributes['contrasena'] = bcrypt($value);
+    }
+
+    public function isActive()
+    {
+        if ($this->fecha_fin && $this->fecha_fin < now()->toDateString()) {
+            return false;
+        }
+        return true;
+    }
+}
